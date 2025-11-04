6ee4f47a74e95471f0fa4bda77e1fd2ef0042c62
sharmayogesh152004@gmail.com <sharmayogesh152004@gmail.com>
2025-11-04 14:51:52 +0530

    update reade me

--- 07_javascript/day_34_assigment/08_question.js
+++ 07_javascript/day_34_assigment/08_question.js
@@ -1,5 +1,5 @@
 //Qustion:8
 
 for(let i=1; i<11; i++){
-    console.log(`5x${i} = ${5*i}`)
+    console.log(`5 x ${i} = ${5*i}`)
 }
--- 07_javascript/day_34_assigment/09_question.js
+++ 07_javascript/day_34_assigment/09_question.js
@@ -1,7 +1,12 @@
 //Qustion:9
 
+let count = 0;
+
 for(let i=1; i<16; i++){
     if(i>8){
+        count++;
         console.log(i)
     }
 }
+
+console.log(`total count is ${count}`);
